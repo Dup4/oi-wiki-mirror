ace1ba3198ba3a08f73a2985d2a3021a20bd7e46
Dup4 <lyuzhi.pan@gmail.com>
2022-06-01 20:55:01 +0800

    chore: use mathjax-render-for-mkdocs-material

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cp -a ./oi-wiki/docs docs
 python3 get-nav.py
 mkdocs build -v
-npx mkdocs-render-math-ssr --srcDir=./site --useWorker
+npx mathjax-render-for-mkdocs-material --srcDir=./site --useWorker
